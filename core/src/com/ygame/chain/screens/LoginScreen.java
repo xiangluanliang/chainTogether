c3f5cd7b7f0385151250d878531362527798ac44
orangelxl741216@gmail.com <orangelxl741216@gmail.com>
2024-07-19 20:20:01 +0800

    LoginScreen

--- core/src/com/ygame/chain/screens/LoginScreen.java
+++ core/src/com/ygame/chain/screens/LoginScreen.java
@@ -3,7 +3,9 @@ package com.ygame.chain.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -23,49 +25,76 @@ import com.kotcrab.vis.ui.VisUI;
  */
 public class LoginScreen implements Screen {
     private Stage stage;
-    private TextField idField;
-    private TextField passwordField;
-    private TextButton loginButton;
-    private TextButton registerButton;
+    private Texture backgroundTexture;
+    private Image backgroundImage1;
+    private Image backgroundImage2;
     Game game;
 
     public LoginScreen(Game game) {
+        this.game = game;
 //        VisUI.load(Gdx.files.internal("uiresource/uiskin.json")); // 加载VisUI皮肤
 
-        this.game = game;
         VisUI.load(VisUI.SkinScale.X2);
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        Table table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
 
-        idField = new TextField("", VisUI.getSkin());
+        backgroundTexture = new Texture(Gdx.files.internal("./resourse/backgrounds/3.png"));
+        backgroundImage1 = new Image(backgroundTexture);
+        backgroundImage2 = new Image(backgroundTexture);
+        backgroundImage1.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        backgroundImage2.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        backgroundImage2.setPosition(Gdx.graphics.getWidth(), 0);
+
+        stage.addActor(backgroundImage1);
+        stage.addActor(backgroundImage2);
+
+        // 创建标题
+        Label titleLabel = new Label("Chain Together", VisUI.getSkin());
+        titleLabel.setColor(Color.BLACK);
+        titleLabel.setFontScale(3);
+
+        // 创建输入字段和按钮
+        TextField idField = new TextField("", VisUI.getSkin());
         idField.setMessageText("Enter ID");
-        passwordField = new TextField("", VisUI.getSkin());
+
+        Label idText = new Label("ID:", VisUI.getSkin());
+        idText.setColor(Color.BLACK);
+
+        TextField passwordField = new TextField("", VisUI.getSkin());
         passwordField.setMessageText("Enter Password");
         passwordField.setPasswordMode(true);
         passwordField.setPasswordCharacter('*');
 
-        loginButton = new TextButton("Login", VisUI.getSkin());
-        registerButton = new TextButton("Register", VisUI.getSkin());
+        Label passwordText = new Label("Password:", VisUI.getSkin());
+        passwordText.setColor(Color.BLACK);
 
-        table.add(new Label("ID:", VisUI.getSkin()));
-        table.add(idField).fillX().uniformX();
-        table.row().pad(10, 0, 10, 0);
-        table.add(new Label("Password:", VisUI.getSkin()));
-        table.add(passwordField).fillX().uniformX();
-        table.row().pad(10, 0, 10, 0);
-        table.add(loginButton).colspan(2).center();
-        table.row().pad(10, 0, 10, 0);
+        TextButton loginButton = new TextButton("Login", VisUI.getSkin());
+        TextButton registerButton = new TextButton("Register", VisUI.getSkin());
+
+        // 创建表格布局
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(titleLabel).colspan(2).center().padBottom(50);
+        table.row();
+        table.add(idText).left();
+        table.add(idField).fillX().uniformX().padBottom(10);
+        table.row();
+        table.add(passwordText).left();
+        table.add(passwordField).fillX().uniformX().padBottom(20);
+        table.row();
+        table.add(loginButton).colspan(2).center().padBottom(10);
+        table.row();
         table.add(registerButton).colspan(2).center();
+
+        stage.addActor(table);
 
         loginButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 String id = idField.getText();
                 String password = passwordField.getText();
+//                Gdx.app.log("aaaa","aaa");
                 // Send login request to server
 
                 game.setScreen(new Level1());
@@ -79,7 +108,6 @@ public class LoginScreen implements Screen {
                 String password = passwordField.getText();
                 // Send register request to server
 
-
             }
         });
     }
@@ -90,13 +118,30 @@ public class LoginScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+
+        // 动态背景效果
+        float speed = 100 * delta; // 背景移动速度
+        backgroundImage1.setX(backgroundImage1.getX() - speed);
+        backgroundImage2.setX(backgroundImage2.getX() - speed);
+
+        // 循环背景
+        if (backgroundImage1.getX() + backgroundImage1.getWidth() <= 0) {
+            backgroundImage1.setX(backgroundImage2.getX() + backgroundImage2.getWidth());
+        }
+        if (backgroundImage2.getX() + backgroundImage2.getWidth() <= 0) {
+            backgroundImage2.setX(backgroundImage1.getX() + backgroundImage1.getWidth());
+        }
+
+        stage.act(Math.min(delta, 1 / 60f));
+//        stage.act(delta);
         stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+        backgroundImage1.setSize(width, height);
+        backgroundImage2.setSize(width, height);
     }
 
     @Override
@@ -111,6 +156,7 @@ public class LoginScreen implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
+        backgroundTexture.dispose();
         VisUI.dispose(); // 释放VisUI资源
     }
 }
